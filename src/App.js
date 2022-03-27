d1b9c69ce164ac36bfa3ff8eb58f05f9d63b2d90
51p50x <51plog50@gmail.com>
2022-03-26 18:34:21 -0600

    created validations with yup library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import {Formik, Form} from 'formik'
+import * as Yup from 'yup'
 import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container.js'
 import Section from './components/Section'
-import { useState } from 'react'
 import Balance from './components/Balance.js'
 
 const compoundInterest = (deposit, contribution, years, rate) => {
@@ -42,13 +43,37 @@ const App = () => {
             rate: '',
           }}
           onSubmit = {handleSubmit}
+          validationSchema ={Yup.object({
+            deposit: Yup
+              .number()
+              .required('You should add a Deposit Number')
+              .typeError('Must be a number value')
+              .min(0, 'Min value is 0'),
+            contribution: Yup
+              .number()
+              .required('You should add a Contribution Number')
+              .typeError('Must be a number value')
+              .min(0, 'Min value is 0'),
+            years: Yup
+              .number()
+              .required('You should add a Years Number')
+              .typeError('Must be a number value')
+              .min(1, 'Min value is 1')
+              .max(20, 'Max value is 20'),
+            rate: Yup
+              .number()
+              .required('You should add a Rate Number')
+              .typeError('Must be a number value')
+              .min(0, 'Min value is 0')
+              .max(1, 'Max value is 1'),
+          })}
         >
           <Form>
             <Input name = "deposit" label="Initial Deposit"/>
             <Input name = "contribution" label="Anual Contribution"/>
             <Input name = "years" label="Years"/>
             <Input name = "rate" label="Interest Rate"/>
-            <Button>Calculate</Button>
+            <Button type = "submit">Calculate</Button>
           </Form>
         </Formik>
         {balance != '' ? <Balance>Final Balance: {balance}</Balance>:null}
